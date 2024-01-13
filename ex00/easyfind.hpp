b31cf8866f70379eadf34911e6aadbb5b882b8a1
Yassine Idrissi <yaidriss@c2r3p4.1337.ma>
2024-01-13 18:20:44 +0100

    add main funtion

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,17 +6,27 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:57:25 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/10 13:15:49 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/13 18:17:32 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <string.h>
+#include <vector>
 
 template <typename T>
 
-easyfind(const T)
+typename T::iterator easyfind(T& container, int index)
 {
-	
+    return std::find(container.begin(), container.end(), index);
 }
+// int easyfind(const T container, int index)
+// {
+// 	for(typename T::iterator i = container.begin(); i != container.end(); i++)
+// 	{
+// 		if(*i == index)
+// 			return (index);
+// 	}
+// 	throw "Not Found";
+// }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,30 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:47:17 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/10 12:55:57 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/13 18:20:28 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+template <typename T>
+typename T::iterator easyfind(T& container, int index);
+
 int main()
-{
-	
+{	
+    std::vector<int> myContainer = {1, 2, 3, 4, 5};
+
+    int valueToFind = 3;
+    auto it = easyfind(myContainer, valueToFind);
+
+    if (it != myContainer.end())
+    {
+        std::cout << "Found " << valueToFind << " at position " << std::distance(myContainer.begin(), it) << "\n";
+    }
+    else
+    {
+        std::cout << "Did not find " << valueToFind << "\n";
+    }
+
+    return 0;
 }
