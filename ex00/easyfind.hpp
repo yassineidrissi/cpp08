f5cf4569d642d21e129b8b121947b43cf50952e6
Yassine Idrissi <yaidriss@c2r3p4.1337.ma>
2024-01-18 18:47:11 +0100

    fix ex00 erreur

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:57:25 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/13 18:17:32 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/18 18:44:44 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,15 @@
 
 #include <string.h>
 #include <vector>
-
+#include <iostream>
 template <typename T>
 
 typename T::iterator easyfind(T& container, int index)
 {
-    return std::find(container.begin(), container.end(), index);
+    typename T::iterator it = std::find(container.begin(), container.end(), index);
+    if (it != container.end()) {
+        return it;
+    } else {
+        throw "Not Found";
+    }
 }
-// int easyfind(const T container, int index)
-// {
-// 	for(typename T::iterator i = container.begin(); i != container.end(); i++)
-// 	{
-// 		if(*i == index)
-// 			return (index);
-// 	}
-// 	throw "Not Found";
-// }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,30 +6,35 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:47:17 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/13 18:20:28 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/18 18:46:31 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-template <typename T>
-typename T::iterator easyfind(T& container, int index);
+
+// template <typename T>
+// typename T::iterator easyfind(T& container, int index);
 
 int main()
 {	
-    std::vector<int> myContainer = {1, 2, 3, 4, 5};
+    std::vector<int> myCont;// = {1, 2, 3, 4, 5};
+	myCont.push_back(1);
+	myCont.push_back(2);
+	myCont.push_back(3);
+	myCont.push_back(4);
+	myCont.push_back(5);
+	myCont.push_back(6);
 
     int valueToFind = 3;
-    auto it = easyfind(myContainer, valueToFind);
-
-    if (it != myContainer.end())
+    try
     {
-        std::cout << "Found " << valueToFind << " at position " << std::distance(myContainer.begin(), it) << "\n";
+        std::vector<int>::iterator it = easyfind(myCont, valueToFind);
+        std::cout << "Found " << valueToFind << " at position " << std::distance(myCont.begin(), it) << "\n";
     }
-    else
+    catch(const char *msg)
     {
         std::cout << "Did not find " << valueToFind << "\n";
     }
-
     return 0;
 }
