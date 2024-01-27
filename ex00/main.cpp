b882bdc28c3e6588e8ebd40c07ce4e44cd75c240
Yassine Idrissi <yaidriss@c2r3p4.1337.ma>
2024-01-28 00:52:23 +0100

    delete all comments

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,30 +6,26 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:47:17 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/28 00:37:07 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/28 00:50:31 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-
-// template <typename T>
-
 int main()
 {	
-    std::vector<int> myCont;// = {1, 2, 3, 4, 5};
-	// myCont.push_back(1);
-	// myCont.push_back(2);
-	// myCont.push_back(3);
-	// myCont.push_back(4);
-	// myCont.push_back(5);
-	// myCont.push_back(6);
-    for (int i = 0; i < 159; i++)
-        myCont.push_back(i*17);
+    std::vector<int> myCont;
+	myCont.push_back(1);
+	myCont.push_back(2);
+	myCont.push_back(3);
+	myCont.push_back(4);
+	myCont.push_back(5);
+	myCont.push_back(6);
+
     int valueToFind = 1700;
     try
     {
-        std::vector<int>::iterator it = easyfind(myCont, valueToFind);
+        __unused std::vector<int>::iterator it = easyfind(myCont, valueToFind);
         std::cout << "Found " << valueToFind << "\n";
     }
     catch(const char *msg)
